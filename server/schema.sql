aca0e9929f2b0ab6ff1c1d383109c833b657dca9
ephraimg <ephraim.glick@gmail.com>
2017-11-03 16:21:05 -0700

    creating database tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,53 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `messageText` VARCHAR(250) NOT NULL DEFAULT 'Yo!',
+  `id_users` INTEGER NULL DEFAULT NULL,
+  `id_rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'Users'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `userName` VARCHAR(50) NOT NULL DEFAULT 'Steve',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `roomName` VARCHAR(50) NOT NULL DEFAULT 'lobby',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
 
 
 
