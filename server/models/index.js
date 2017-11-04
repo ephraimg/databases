4e38afb04e711714915b1a87e7c08354f9351f37
ephraimg <ephraim.glick@gmail.com>
2017-11-03 21:07:08 -0700

    Complete models get and post message functions

--- server/models/index.js
+++ server/models/index.js
@@ -2,18 +2,16 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
-      db.conn.query('select * from `messages`', function(err, results, fields) {
-        if (err) { console.error(err); }
-        console.log('Results:\n', results);
-      });
+    get: function (callback) {
+      db.conn.query('select * from `messages`', callback);
     }, // a function which produces all the messages
-    post: function () {
-      db.conn.query("insert into messages (messageText, id_users, id_rooms) values ('Another message from Steve...', 1, 1)",
-        function(err, results, fields) {
-          if (err) { console.error(err); }
-          console.log('Successfully inserted message!'); 
-        });
+    post: function (message, callback) {
+      var mysqlObj = {
+        "text": message.text,
+        "username": message.username,
+        "roomname": message.roomname
+      };
+      db.conn.query("insert into messages set ?", mysqlObj, callback);
     } // a function which can be used to insert a message into the database
   },
 
@@ -22,15 +20,15 @@ module.exports = {
     get: function () {},
     post: function () {}
   }
-};
+}; 
 
-console.log('Attempting to get mysql messages...\n');
-module.exports.messages.post();
-module.exports.messages.post();
-module.exports.messages.post();
-module.exports.messages.post();
-console.log('Attempting to get mysql messages...\n');
-module.exports.messages.get();
+// console.log('Attempting to get mysql messages...\n');
+// module.exports.messages.post();
+// // module.exports.messages.post();
+// // module.exports.messages.post();
+// // module.exports.messages.post();
+// console.log('Attempting to get mysql messages...\n');
+// module.exports.messages.get();
 
 // messages = { {user: 'e', text: 'hi'}, ... }
 // messages[1]
