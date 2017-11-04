6a7f6565dee8e0f406c93539d2138a4d9a16ff5f
ephraimg <ephraim.glick@gmail.com>
2017-11-03 17:45:20 -0700

    Establish mySQL connection, start on get/post

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,24 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'student',
+  password : 'student',
+  database : 'chat'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected! ' + connection.threadId);
+});
+
+// connection.query('describe messages', function(error, results, fields) {
+//   if (error) { console.error(error); }
+//   console.log('The messages table looks like this:\n', results);
+// });
+
+exports.conn = connection;
--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,19 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.conn.query('select * from `messages`', function(err, results, fields) {
+        if (err) { console.error(err); }
+        console.log('Results:\n', results);
+      });
+    }, // a function which produces all the messages
+    post: function () {
+      db.conn.query("insert into messages (messageText, id_users, id_rooms) values ('Another message from Steve...', 1, 1)",
+        function(err, results, fields) {
+          if (err) { console.error(err); }
+          console.log('Successfully inserted message!'); 
+        });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
@@ -13,3 +24,15 @@ module.exports = {
   }
 };
 
+console.log('Attempting to get mysql messages...\n');
+module.exports.messages.post();
+module.exports.messages.post();
+module.exports.messages.post();
+module.exports.messages.post();
+console.log('Attempting to get mysql messages...\n');
+module.exports.messages.get();
+
+// messages = { {user: 'e', text: 'hi'}, ... }
+// messages[1]
+
+// messages.get(1);
