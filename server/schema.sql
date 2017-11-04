8903d609bb8829cdcf51095c73a0904871586b87
ephraimg <ephraim.glick@gmail.com>
2017-11-03 21:02:39 -0700

    Switch to single table database for MVP

--- server/schema.sql
+++ server/schema.sql
@@ -11,45 +11,11 @@ DROP TABLE IF EXISTS `messages`;
     
 CREATE TABLE `messages` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `messageText` VARCHAR(250) NOT NULL DEFAULT 'Yo!',
-  `id_users` INTEGER NULL DEFAULT NULL,
-  `id_rooms` INTEGER NULL DEFAULT NULL,
+  `text` VARCHAR(255) NOT NULL DEFAULT 'Hello!',
+  `username` VARCHAR(255) NOT NULL DEFAULT 'Anonymous',
+  `roomname` VARCHAR(255) NOT NULL DEFAULT 'lobby',
   PRIMARY KEY (`id`)
 );
-
--- ---
--- Table 'Users'
--- 
--- ---
-
-DROP TABLE IF EXISTS `users`;
-    
-CREATE TABLE `users` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `userName` VARCHAR(50) NOT NULL DEFAULT 'Steve',
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'rooms'
--- 
--- ---
-
-DROP TABLE IF EXISTS `rooms`;
-    
-CREATE TABLE `rooms` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `roomName` VARCHAR(50) NOT NULL DEFAULT 'lobby',
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Foreign Keys 
--- ---
-
-ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
-ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
-
 
 
 /*  Execute this file from the command line by typing:
